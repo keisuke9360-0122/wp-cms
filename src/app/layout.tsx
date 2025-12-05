c44401048534f12938919d400f51eb4406f41c22
keisuke9360-0122 <gz5y85tm3artsy4@i.softbank.jp>
2025-12-06 02:34:39 +0900

    フォント調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { LoadingProvider } from "@/app/contexts/LoadingContext";
 import LoadingOverlay from "@/components/LoadingOverlay";
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: "swap",
 });
+
 export const metadata: Metadata = {
   title: "KEISUKE TSURUTA PORTFOLIO",
   description: "KEISUKE TSURUTA PORTFOLIO",
@@ -20,14 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja" className={`${inter.variable}`}>
-      <body className="relative text-gray-800 font-sans antialiased">
-        {/* ▼ 背景アニメーション */}
+    <html lang="ja" className={inter.variable}>
+      {/* ★ ここで Inter を強制適用 */}
+      <body className="relative text-gray-800 font-inter antialiased">
         <div className="absolute inset-0 -z-10 animate-gradient"></div>
 
-        {/* ▼ コンテンツ */}
         <LoadingProvider>
-          {/* <BodyScrollLock /> */}
           <LoadingOverlay />
           <div className="relative z-20 overflow-x-hidden">{children}</div>
         </LoadingProvider>
