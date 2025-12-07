002770baea6f9590d72eef058bbd513037287e71
keisuke9360-0122 <gz5y85tm3artsy4@i.softbank.jp>
2025-12-07 13:34:34 +0900

    修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={inter.variable}>
       {/* ★ ここで Inter を強制適用 */}
       <body className="relative text-gray-800 font-inter antialiased">
         <div className="absolute inset-0 -z-10 animate-gradient"></div>
