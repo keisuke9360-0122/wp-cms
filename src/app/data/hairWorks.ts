50c41b2d23242a92fe97e28cdeb68b98c81cc6b2
keisuke9360-0122 <gz5y85tm3artsy4@i.softbank.jp>
2025-12-26 00:35:35 +0900

    画像変更

--- src/app/data/hairWorks.ts
+++ src/app/data/hairWorks.ts
@@ -1,5 +1,5 @@
 export const hairWorks = [
-  { id: 1, src: "/hair/style5.jpeg", title: "Cut" },
+  { id: 1, src: "/hair/style6.jpeg", title: "Cut" },
   { id: 2, src: "/hair/style4.jpeg", title: "Color" },
   { id: 3, src: "/hair/style3.jpeg", title: "Styling" },
 ];
